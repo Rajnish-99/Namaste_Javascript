099b6671df3b546df96773a391f46d14545623eb
rajnish ranjan <ranjanrajnish64@gmail.com>
2024-06-16 14:11:28 +0530

    Async Functions are defined

---
+++ async_await.js
@@ -0,0 +1,30 @@
+// async function
+
+async function getData() {
+  return "Rajnish Ranjan is my name";
+}
+
+// we will get promise when you will call this function because this is a async function
+const dataPromise = getData();
+
+console.log(dataPromise);
+
+dataPromise.then((res) => console.log(res));
+
+
+// will return a promise and lets test that 
+
+
+const p = new Promise((resolve , reject) =>{
+    resolve("Promise Resolved");
+})
+
+
+async function getPromise (){
+    return p;  // the data p will be as it is because it is a promise it need not to be wrapped inside anything 
+}
+
+
+const promiseCheck = getPromise ();
+
+promiseCheck.then((res) => console.log(res));
